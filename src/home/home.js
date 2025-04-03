ce764e5eb6c9dc6548a71cba68a35466aa8b7536
epsl.kwm@gmail.com <100332811+SeungYeon04@users.noreply.github.com>
2025-04-03 13:27:32 +0900

    g

--- src/home/home.js
+++ src/home/home.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import person from "../image/임시.jpg";
 import Career from "./career";
 
-const Stacks = ["All", "Unity", "C#", "React", "Node.js", "Python", "Django"];
+const Stacks = ["All", "Unity", "C#", "Js", "React", "Kotlin", "Java", "JSP"];
 
 function StackFilter({ selStack, onChange }) {
   const toggleStack = (tech) => {
@@ -39,29 +39,37 @@ export default function Home() {
 
   const careerList = [
     {
-      title: "터치형 RPG",
+      title: "공부정리 블로그",
+      subtitle: "개인 프로젝트",
+      date: "22.02 ~ 22.12",
+      comment: "대학교를 다니며 만든 공부 정리 사이트. 해시태그를 사용하여 렌더링하며 github.io 전용 블로그로 만들었다.",
+      img: "/Image/studyBlog.png",
+      Stack: ["React"],
+    },
+    {
+      title: "새 터치형 Open RPG",
       subtitle: "개인 프로젝트",
       date: "21.02 ~ 21.02",
-      comment: "몬스터를 터치하여 잡는 간단한 모바일 RPG",
-      img: "/Image/temp.jpg",
+      comment: "모바일 터치형 Open RPG, 낚시 수집 사냥 꾸미기 등 여러 콘텐츠가 존재함. 최종목표는 P2P 등으로 멀티구현하는 것이다.",
+      img: "/Image/birdGame.jpg",
       Stack: ["Unity", "C#"],
     },
     {
-      title: "개발자2",
-      subtitle: "개인 프로젝트",
-      date: "22.02 ~ 22.12",
-      comment: "웹 포트폴리오 제작 프로젝트",
-      img: "/Image/temp.jpg",
-      Stack: ["React", "Node.js"],
+      title: "간단한 로고 공식사이트",
+      subtitle: "개인프로젝트",
+      date: "23.01 ~ 23.06",
+      comment: "나중에 게임개발 끝나면, 사람들이 게임을 쉽게 찾을 수 있도록 열어둘 게임 공식 사이트. 처음에는 대학교 리눅스 수업 때 버츄얼박스로 호스팅하여 쓰던 웹이다.",
+      img: "/Image/linuxWeb.png",
+      Stack: ["HTML", "CSS", "Js"],
     },
     {
-      title: "제목3",
-      subtitle: "팀 프로젝트",
+      title: "토끼 일기 & 메모 사이트",
+      subtitle: "개인프로젝트",
       date: "23.01 ~ 23.06",
-      comment: "장고를 이용한 백엔드 시스템 구축",
-      img: "/Image/temp.jpg",
-      Stack: ["Python", "Django"],
-    },
+      comment: "파이어베이스를 이용해 로그인과 글작성 구현. 디자인 비율 등 보완이 필요한 상태이다.",
+      img: "/Image/RabbitBlog.png",
+      Stack: ["HTML", "CSS", "Js", "Firebace"],
+    }
   ];
 
   const filteredCareers =
