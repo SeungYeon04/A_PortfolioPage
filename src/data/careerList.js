486e66904e166fdc1756ac64e7a49ec11b631e1d
epsl.kwm@gmail.com <100332811+SeungYeon04@users.noreply.github.com>
2025-04-05 00:21:34 +0900

    .

---
+++ src/data/careerList.js
@@ -0,0 +1,59 @@
+// src/data/careerList.js
+
+const careerList = [
+    {
+      title: "공부정리 블로그",
+      subtitle: "개인 프로젝트",
+      date: "24.XX ~ 24.XX",
+      comment: "대학교를 다니며 만든 공부 정리 사이트...",
+      img: "/Image/studyBlog.png",
+      Stack: ["React"],
+      url: "https://github.com/SeungYeon04/A_Study"
+    },
+    {
+      title: "새 터치형 Open RPG",
+      subtitle: "개인 프로젝트",
+      date: "24.XX ~ 25.XX",
+      comment: "모바일 터치형 Open RPG 게임...",
+      img: "/Image/birdGame.jpg",
+      Stack: ["Unity", "C#", "Json"]
+    },
+    {
+      title: "끄아 웹게임",
+      subtitle: "팀프로젝트 (프론트2/백1)",
+      date: "25.XX ~ 25.XX",
+      comment: "끝말잇기 아케이드로 웹게임을 개발중...",
+      img: "/Image/kkuaWeb.png",
+      Stack: ["React", "Docker", "git", "Figma"]
+    },
+    {
+      title: "간단한 게임 공식사이트",
+      subtitle: "개인프로젝트",
+      date: "24.XX ~ 24.XX",
+      comment: "나중에 게임개발 끝나면 쓸까 하는 게임 공식 사이트 디자인...",
+      img: "/Image/linuxWeb.png",
+      Stack: ["HTML", "CSS"],
+      url: "https://github.com/SeungYeon04/A_Linux.github.io"
+    },
+    {
+      title: "토끼 일기 & 메모 사이트",
+      subtitle: "개인프로젝트",
+      date: "24.XX ~ 24.XX",
+      comment: "파이어베이스를 이용해 로그인과 글작성 구현...",
+      img: "/Image/RabbitBlog.png",
+      Stack: ["HTML", "CSS", "Js", "Firebace"],
+      url: "https://github.com/SeungYeon04/A_RabbitBLOG"
+    },
+    {
+      title: "수박게임 모작",
+      subtitle: "팀프로젝트",
+      date: "23.XX ~ 23.XX",
+      comment: "유니티를 사용하고 코딩과 디자인을 맡았다...",
+      img: "/Image/wMGame.png",
+      Stack: ["Unity", "C#", "MediBang", "MiriCanvas"],
+      url: "https://github.com/SeungYeon04/C_MergeGame"
+    }
+  ];
+  
+  export default careerList;
+  
--- src/home/home.js
+++ src/home/home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Star01 from "../stars/star01"; 
 import Star02 from "../stars/star02";
+import careerList  from "../data/careerList";
 
 const Stacks = ["All", "Unity", "C#", "Js", "React", "Kotlin", "Java", "JSP"];
 
@@ -37,52 +38,7 @@ function StackFilter({ selStack, onChange }) {
 export default function Home() {
   const [selStack, setSelStack] = useState([]);
 
-  const careerList = [
-    {
-      title: "공부정리 블로그",
-      subtitle: "개인 프로젝트",
-      date: "24.XX ~ 24.XX",
-      comment: "대학교를 다니며 만든 공부 정리 사이트. 해시태그를 사용하여 markdown file을 렌더링하며 github.io 전용 블로그로 만들었다.",
-      img: "/Image/studyBlog.png",
-      Stack: ["React"],
-      url: "https://github.com/SeungYeon04/A_Study"
-    },
-    {
-      title: "새 터치형 Open RPG",
-      subtitle: "개인 프로젝트",
-      date: "24.XX ~ 25.XX",
-      comment: "모바일 터치형 Open RPG 게임, 낚시 수집 사냥 꾸미기 등 여러 콘텐츠가 존재함. 최종목표는 P2P 등으로 멀티구현하는 것이다.",
-      img: "/Image/birdGame.jpg",
-      Stack: ["Unity", "C#", "Json"],
-    },
-    {
-      title: "끄아 웹게임",
-      subtitle: "팀프로젝트 (프론트2/백1)",
-      date: "25.XX ~ 25.XX",
-      comment: "끝말잇기 아케이드로 웹게임을 개발중. 본인은 프론트엔드의 디자인, UI화면, 기능 등을 담당하였다.",
-      img: "/Image/kkuaWeb.png",
-      Stack: ["React", "Docker", "git", "Figma"],
-    },
-    {
-      title: "간단한 게임 공식사이트",
-      subtitle: "개인프로젝트",
-      date: "24.XX ~ 24.XX",
-      comment: "나중에 게임개발 끝나면 쓸까 하는 게임 공식 사이트 디자인. 처음엔 대학교 수업으로 우분투 + 호스팅 테스트로 쓰던 웹이다.",
-      img: "/Image/linuxWeb.png",
-      Stack: ["HTML", "CSS"],
-      url: "https://github.com/SeungYeon04/A_Linux.github.io"
-    },
-    {
-      title: "토끼 일기 & 메모 사이트",
-      subtitle: "개인프로젝트",
-      date: "24.XX ~ 24.XX",
-      comment: "파이어베이스를 이용해 로그인과 글작성 구현. 디자인 비율 등 보완이 필요한 상태이다.",
-      img: "/Image/RabbitBlog.png",
-      Stack: ["HTML", "CSS", "Js", "Firebace"],
-      url: "https://github.com/SeungYeon04/A_RabbitBLOG"
-    },
-  ];
-
+  //const careerList 를 모듈화해서 import해서 careerList 이름 그대로 전달 
   const filteredCareers =
     selStack.length === 0 || selStack.includes("All")
       ? careerList
