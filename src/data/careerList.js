760bd294fadf4c012ac839e920b798d924bbb5ad
epsl.kwm@gmail.com <100332811+SeungYeon04@users.noreply.github.com>
2025-04-29 15:50:35 +0900

    fix project data addtion

--- src/data/careerList.js
+++ src/data/careerList.js
@@ -2,8 +2,8 @@ const careerList = [
     {
       title: "공부정리 블로그",
       subtitle: "개인 프로젝트",
-      date: "24.XX ~ 24.XX",
-      comment: "대학교를 다니며 만든 공부 정리 사이트. 해시태그를 사용하여 markdown file을 렌더링하며 github.io 전용 블로그로 만들었다.",
+      date: "2024. ~ 2024.",
+      comment: "대학을 다니며 만든 공부 정리 사이트. 해시함수를 사용하여 markdown file을 렌더링하며 github.io 전용 블로그",
       img: "/Image/studyBlog.png",
       Stack: ["React"],
       url: "https://github.com/SeungYeon04/A_Study"
@@ -11,37 +11,65 @@ const careerList = [
     {
       title: "새 터치형 Open RPG",
       subtitle: "개인 프로젝트",
-      date: "24.XX ~ 25.XX",
-      comment: "모바일 터치형 Open RPG 게임, 낚시 수집 사냥 꾸미기 등 여러 콘텐츠가 존재함. 최종목표는 P2P 등으로 멀티구현하는 것이다.",
+      date: "2024. ~ 2025.",
+      comment: "모바일 터치형 Open RPG 게임, 낚시 수집 사냥 꾸미기 등 여러 콘텐츠가 존재. 최종목표는 P2P 등으로 멀티구현",
       img: "/Image/birdGame.jpg",
       Stack: ["Unity", "C#", "Json"],
     },
     {
       title: "끄아 웹게임",
       subtitle: "팀프로젝트 (프론트2/백1)",
-      date: "25.XX ~ 25.XX",
-      comment: "끝말잇기 아케이드로 웹게임을 개발중. 본인은 프론트엔드의 디자인, UI화면, 기능 등을 담당하였다.",
+      date: "2025. ~ 2025.",
+      comment: "끝말잇기 아케이드 웹게임이다. 대학교를 다니며 UI디자인, InGame 로직과 웹소켓 틀 등을 담당",
       img: "/Image/kkuaWeb.png",
       Stack: ["React", "Docker", "git", "Figma"],
+      url: "https://github.com/djgnfj-svg/kkua"
+    },
+    {
+      title: "토끼 일기 & 메모 사이트",
+      subtitle: "개인프로젝트",
+      date: "2024. ~ 2024.",
+      comment: "파이어베이스를 이용해 로그인과 글작성 구현. 디자인 비율 등 보완이 필요한 상태",
+      img: "/Image/RabbitBlog.png",
+      Stack: ["HTML", "CSS", "Js", "Firebace"],
+      url: "https://github.com/SeungYeon04/A_RabbitBLOG"
     },
     {
       title: "간단한 게임 공식사이트",
       subtitle: "개인프로젝트",
-      date: "24.XX ~ 24.XX",
-      comment: "나중에 게임개발 끝나면 쓸까 하는 게임 공식 사이트 디자인. 처음엔 대학교 수업으로 우분투 + 호스팅 테스트로 쓰던 웹이다.",
+      date: "2024. ~ 2024.",
+      comment: "나중에 게임개발 끝나면 쓸까 하는 게임 공식 사이트 디자인. 처음엔 대학교 수업으로 우분투 + 호스팅 테스트로 쓰던 웹",
       img: "/Image/linuxWeb.png",
       Stack: ["HTML", "CSS"],
       url: "https://github.com/SeungYeon04/A_Linux.github.io"
     },
     {
-      title: "토끼 일기 & 메모 사이트",
+      title: "아쿠아포닉스 소개사이트",
       subtitle: "개인프로젝트",
-      date: "24.XX ~ 24.XX",
-      comment: "파이어베이스를 이용해 로그인과 글작성 구현. 디자인 비율 등 보완이 필요한 상태이다.",
-      img: "/Image/RabbitBlog.png",
+      date: "2024. ~ 2024.",
+      comment: "아쿠아포닉스 소개 + 로그인 + 게시판 + 상품소개 등 로직 위주 웹이며, 교내 사업아이템 경진대회 발표용 웹",
+      img: "/Image/AquaponicsWeb.png",
       Stack: ["HTML", "CSS", "Js", "Firebace"],
-      url: "https://github.com/SeungYeon04/A_RabbitBLOG"
+      url: "https://github.com/SeungYeon04/A_Aquaponics"
     },
+    {
+      title: "수박게임 모작제작",
+      subtitle: "팀프로젝트",
+      date: "2023. ~ 2024.",
+      comment: "스파르타 코딩클럽에서 작업한 수박모작게임. 전체 그림&UI와 시작페이지와 충돌과 간단 로직을 작업",
+      img: "/Image/wMGame.png",
+      Stack: ["Unity", "C#"],
+      url: ""
+    }, 
+    {
+      title: "토깽이 MBTI 테스트",
+      subtitle: "개인프로젝트",
+      date: "2022. ~ 2023.",
+      comment: "MBTI 테스트 유행할 때 강의 따라서 만든 간단한 MBTI기반 토깽이 유형 테스트",
+      img: "/Image/rabbitTest.png",
+      Stack: ["HTML", "CSS", "Js"],
+      url: "https://rabbit8mbtitest.netlify.app/"
+    }, 
   ];
   
   export default careerList;
